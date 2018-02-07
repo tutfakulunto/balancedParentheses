438fa3c0891a112b4cc4bc4886bbb4975a547066
Scott Johnson <c.scott.johnson@gmail.com>
2018-02-07 12:04:56 -0600

    first commit

---
+++ balancedParentheses.js
@@ -0,0 +1,8 @@
+function balancedParentheses(string) {
+    // if 0 is returned, this means parentheses are in fact balanced
+    return !string.split("").reduce(function(previous, char) {
+        if (char === "(") { return ++previous; }
+        if (char === ")") { return --previous; }
+        return previous;
+    }, 0);
+}
