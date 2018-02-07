861bc55dc5c1af712a4e93bbd3b0882922610e3c
Scott Johnson <c.scott.johnson@gmail.com>
2018-02-07 12:06:26 -0600

    added catch for less than zero

--- balancedParentheses.js
+++ balancedParentheses.js
@@ -1,6 +1,7 @@
 function balancedParentheses(string) {
     // if 0 is returned, this means parentheses are in fact balanced
     return !string.split("").reduce(function(previous, char) {
+        if previous < 0) {return previous; }
         if (char === "(") { return ++previous; }
         if (char === ")") { return --previous; }
         return previous;
